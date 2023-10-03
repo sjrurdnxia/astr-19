3b8a388869552081f138272402e81da9444f2d03
sjrurdnxia <73916353+sjrurdnxia@users.noreply.github.com>
2023-10-03 12:54:28 -0700

    Create Session1Prompt.py

---
+++ Session1Prompt.py
@@ -0,0 +1,22 @@
+#This will probably print hello world!
+
+def PrintName():
+	print("Allan Dewey")
+def Pronouns():
+	print("he/him")
+def Favorites():
+  print("Rush Hour")
+  print("Hot Pot")
+
+
+
+
+
+
+def main():
+	PrintHelloWorld()
+	Pronouns()
+  Favorites()
+
+if __name__=="__main__":
+	main()
